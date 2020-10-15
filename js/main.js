2bb666948de13a87633b4d85d092b7e97a8d840a
rejth <kirsanovia@yandex.ru>
2020-10-15 22:11:21 +0300

    update: finished reviews section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
-  var tabsItem = $(".trends__tabs-item");
-  var contentItem = $(".trends__cards-wrapper");
+  // Review slider
+  let reviewSlider = new Swiper('.reviews-slider', {
+    autoHeight: true,
+    loop: true,
+    slidesPerView: 1,
+    keyboard: {
+      enabled: true
+    },
+    autoplay: {
+      delay: 7000
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    mousewheel: {
+      invert: true
+    }
+  });
+
+  // Trends section tabs
+  let tabsItem = $(".trends__tabs-item");
+  let contentItem = $(".trends__cards-wrapper");
 
   tabsItem.on("click", function (e) {
-    var activeContent = $(this).attr("data-target");
+    let activeContent = $(this).attr("data-target");
     tabsItem.removeClass("trends__tabs-item--active");
     contentItem.removeClass("trends__cards-wrapper--active");
     $(activeContent).addClass("trends__cards-wrapper--active");
