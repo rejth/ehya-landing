becf0f732be8fdaa803fee15d5638dc0e2cada3b
rejth <kirsanovia@yandex.ru>
2020-10-17 00:29:35 +0300

    update: added newsletter block

--- js/main.js
+++ js/main.js
@@ -34,7 +34,6 @@ $(document).ready(function () {
   // Trends section tabs
   let tabsItem = $(".trends__tabs-item");
   let contentItem = $(".trends__cards-wrapper");
-
   tabsItem.on("click", function (e) {
     let activeContent = $(this).attr("data-target");
     tabsItem.removeClass("trends__tabs-item--active");
@@ -42,4 +41,52 @@ $(document).ready(function () {
     $(activeContent).addClass("trends__cards-wrapper--active");
     $(this).addClass("trends__tabs-item--active");
   });
+
+  $('img.img-svg').each(function(){
+    var $img = $(this);
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+    $.get(imgURL, function(data) {
+      var $svg = $(data).find('svg');
+      if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+      }
+      $svg = $svg.removeAttr('xmlns:a');
+      if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+      $img.replaceWith($svg);
+    }, 'xml');
+  });
+
+  // Stories section buttons
+  let storyButton = $(".stories__button");
+  let arrowSvg = $(".img-svg");
+  storyButton.on("click", function(e) {
+    let activeArrow = $(this).attr("data-target");
+    storyButton.removeClass("stories__button--active");
+    arrowSvg.removeClass("img-svg--active");
+    $(activeArrow).addClass("img-svg--active");
+    $(this).addClass("stories__button--active");
+  });
+
+  // Form Validator
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        email: {
+          required: "Нам нужна Ваша электронная почта, чтобы связаться с вами",
+          email: "Ваша электронная почта должна быть в формате name@domain.com"
+        }
+      }
+    });
+  });
+
 });
